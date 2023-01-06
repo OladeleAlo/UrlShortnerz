8763a8d4a70628ac38b957514a1494bf3c2938a6
Oladele <oladele2alo@gmail.com>
2023-01-06 13:10:59 +0100

    Shorten any url

---
+++ src/BackgrounImage.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const BackgrounImage = () => {
+  return (
+    <div className="background">
+      <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default BackgrounImage;
